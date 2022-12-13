a65c9f495489b9bf16cbbb5e0b480c887c865535
Przemyslaw Skowronski <p.skowronski120@gmail.com>
2022-12-13 04:18:42 +0100

    lease enter the commit message for your changes. Lines starting

--- js/script.js
+++ js/script.js
@@ -8,32 +8,34 @@ welcome();
         const button1 = document.querySelector(".js-header__buttonHeading");
         const header__heading = document.querySelector(".js-header__heading");
         header__heading.remove();
-    })
+    };
+
+    const removeImage() {
+        const button2 = document.querySelector(".js-header__buttonImage");
+        const img = document.querySelector(".js-header__image");
+        img.remove();
+    }
+
+    const toggleColor() {
+        const button3 = document.querySelector(".js-header__buttonColor");
+        const body = document.querySelector(".js-body");
+        const themeName = document.querySelector(".themeName");
+        body.classList.toggle("dark");
+
+        if (body.classList.contains("dark")) {
+            themeName.innerText = "jasny";
+        } else {
+            themeName.innerText = "ciemny";
+        };
+
+        welcome();
+
+        button1.addEventListener(removeHeading);
+
+        button2.addEventListener(removeImage);
+
+        button3.addEventListener(toggleColor);
 
 
-
-let button2 = document.querySelector(".js-header__buttonImage");
-let img  = document.querySelector(".js-header__image");
-let button3 = document.querySelector(".js-header__buttonColor");
-let body = document.querySelector(".js-body");
-let themeName = document.querySelector(".themeName");
-
-
-button1.addEventListener("click", (removeHeading) 
-
-button2.addEventListener("click", () => {
-    img .remove();
-})
-
-button3.addEventListener("click", () => {
-    body.classList.toggle("dark");
-
-    if (body.classList.contains("dark")) {
-        themeName.innerText = "jasny";
-} else {
-         themeName.innerText = "ciemny";
-}
-
-
-})
+    }
 }
