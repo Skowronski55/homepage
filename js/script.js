c6fc7481432410c25c9d5d5d99b927e00b6b10ae
Przemyslaw Skowronski <p.skowronski120@gmail.com>
2022-12-17 10:07:49 +0100

    newjs

--- js/script.js
+++ js/script.js
@@ -1,41 +1,28 @@
-{
-    const welcome = () => {
-console.log("Cześć! Witam wszystkich!");
+console.log("Cześć!");
+
+let button1 = document.querySelector(".js-header__buttonHeading");
+let header__heading = document.querySelector(".js-header__heading");
+let button2 = document.querySelector(".js-header__buttonImage");
+let img  = document.querySelector(".js-header__image");
+let button3 = document.querySelector(".js-header__buttonColor");
+let body = document.querySelector(".js-body");
+let themeName = document.querySelector(".themeName");
+
+
+button1.addEventListener("click", () => {
+    header__heading.remove();
+})
+
+button2.addEventListener("click", () => {
+    img .remove();
+})
+
+button3.addEventListener("click", () => {
+    body.classList.toggle("dark");
+
+    if (body.classList.contains("dark")) {
+        themeName.innerText = "jasny";
+} else {
+         themeName.innerText = "ciemny";
 }
-
-
-    const removeHeading = () => {
-        const button1 = document.querySelector(".js-header__buttonHeading");
-        const header__heading = document.querySelector(".js-header__heading");
-        header__heading.remove();
-    };
-
-    const removeImage = () => {
-        const button2 = document.querySelector(".js-header__buttonImage");
-        const img = document.querySelector(".js-header__image");
-        img.remove();
-    }
-
-    const toggleColor = () => {
-        const button3 = document.querySelector(".js-header__buttonColor");
-        const body = document.querySelector(".js-body");
-        const themeName = document.querySelector(".themeName");
-        body.classList.toggle("dark");
-
-        if (body.classList.contains("dark")) {
-            themeName.innerText = "jasny";
-        } else {
-            themeName.innerText = "ciemny";
-        };
-
-        welcome();
-
-        button1.addEventListener(removeHeading);
-
-        button2.addEventListener(removeImage);
-
-        button3.addEventListener(toggleColor);
-
-
-    }
-}
+})
